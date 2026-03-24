ffc8e5488a68f39bf5804affd4984f55a415e719
fgyy-ux <arturcondratov@gmail.com>
2026-03-24 16:39:53 +0200

    small function improvement

--- rpg-fight-code/leveling_system.py
+++ rpg-fight-code/leveling_system.py
@@ -2,7 +2,7 @@ player_xp = 0
 player_level = 1
 def check_xp(player_xp, player_level):
     needed_xp = player_xp * 10
-    while needed_xp >= player_level:
+    if needed_xp >= player_level:
         player_level += 1
         player_xp -= needed_xp
         print(f'You leveled UP! You are now level {player_level}')
