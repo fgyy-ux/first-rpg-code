bf3d949dfd3ee0c24aad0b7904088001e39fb636
fgyy-ux <arturcondratov@gmail.com>
2026-04-05 12:41:47 +0300

    small valuables copied into new files

--- rpg-fight-code/src/player/player.py
+++ rpg-fight-code/src/player/player.py
@@ -2,9 +2,12 @@ class Player:
     def __init__(self):
         Player.hp = 100
         Player.max_hp = 100
+        Player.heal_cooldown = 0
+        Player.heal_random = 0
 
         Player.damage = 10
         Player.fireball_damage = 25
+        Player.fireball_cooldown = 0
         Player.defense = False
 
         Player.xp = 0
